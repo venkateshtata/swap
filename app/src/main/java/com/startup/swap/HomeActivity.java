c78c30951724217adb16597316100c98abe5bd67
venkateshtata <venkateshtata9@gmail.com>
2019-06-19 06:41:09 +0530

    Initial commit

---
+++ app/src/main/java/com/startup/swap/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.startup.swap;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+    }
+
+}
