0a194d0f599f00823c94fa4344549aca962054c3
venkateshtata <venkateshtata9@gmail.com>
2019-06-20 07:20:10 +0530

    added fragments

--- app/src/main/java/com/startup/swap/HomeActivity.java
+++ app/src/main/java/com/startup/swap/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.startup.swap;
 
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -20,6 +22,21 @@ public class HomeActivity extends AppCompatActivity {
 
         drawer = findViewById(R.id.drawer_layout);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(HomeActivity.this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 
+    @Override
+    public void onBackPressed() {
+
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
 }
